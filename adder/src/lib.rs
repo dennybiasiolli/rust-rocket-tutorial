8fa1cd40a1a5bb937a7e6a38e74b1cd6bec711a6
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2024-01-10 10:48:36 +0100

    adding more tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,11 @@ pub fn add(n1: usize, n2: usize) -> usize {
     n1 + n2
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +36,18 @@ mod tests {
     fn it_returns_err() {
         let custom_err: Result<(), String> = Err(String::from("custom error"));
         assert!(custom_err.is_err());
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
