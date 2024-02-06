cbdd1fbbc1b57c03c06d8f0f62463fb36cce0064
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2024-02-06 11:19:51 +0100

    concurrency: adding mutex example

--- concurrency/src/lib.rs
+++ concurrency/src/lib.rs
@@ -42,7 +42,7 @@ pub mod channels {
                 tx.send(val).unwrap();
                 thread::sleep(Duration::from_millis(200));
             }
-        })
+        });
     }
 
     pub fn test1() {
@@ -70,5 +70,42 @@ pub mod channels {
             }
         });
         handle.join().unwrap();
+    }
+}
+
+pub mod mutex {
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+
+    pub fn test1() {
+        let m = Mutex::new(5);
+
+        {
+            let mut num = m.lock().unwrap();
+            *num = 6;
+        }
+
+        println!("m = {:?}", m);
+    }
+
+    pub fn test2() {
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
     }
 }
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,9 +1,13 @@
-use concurrency::threads;
 use concurrency::channels;
+use concurrency::mutex;
+use concurrency::threads;
 
 fn main() {
     threads::test1();
     threads::test2();
 
     channels::test1();
+
+    mutex::test1();
+    mutex::test2();
 }
