850b527c8b3ef0f15311c1e3c217ec9a8fdc5d16
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2024-03-09 11:15:34 +0100

    improving the state machine

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,12 +1,14 @@
 pub struct Post {
     state: Option<Box<dyn State>>,
     content: String,
+    approval_count: u8,
 }
 impl Post {
     pub fn new() -> Post {
         Post {
             state: Some(Box::new(Draft {})),
             content: String::new(),
+            approval_count: 0,
         }
     }
 
@@ -25,8 +27,19 @@ impl Post {
     }
 
     pub fn approve(&mut self) {
+        if self.approval_count < 2 {
+            self.approval_count += 1;
+        }
+        if self.approval_count == 2 {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.approve())
+            }
+        }
+    }
+
+    pub fn reject(&mut self) {
         if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+            self.state = Some(s.reject())
         }
     }
 }
@@ -34,6 +47,7 @@ impl Post {
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
     fn content<'a>(&self, _post: &'a Post) -> &'a str {
         ""
     }
@@ -47,6 +61,9 @@ impl State for Draft {
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
 }
 
 struct PendingReview {}
@@ -57,6 +74,9 @@ impl State for PendingReview {
     fn approve(self: Box<Self>) -> Box<dyn State> {
         Box::new(Published {})
     }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
 }
 
 struct Published {}
@@ -65,6 +85,9 @@ impl State for Published {
         self
     }
     fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
         self
     }
     fn content<'a>(&self, post: &'a Post) -> &'a str {
@@ -86,6 +109,30 @@ mod tests {
         post.request_review();
         assert_eq!("", post.content());
 
+        post.approve();
+        assert_eq!("", post.content());
+        post.approve();
+        assert_eq!("I ate a salad for lunch today", post.content());
+    }
+
+    #[test]
+    fn it_works_with_reject() {
+        let mut post = Post::new();
+
+        post.add_text("I ate a salad for lunch today");
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.reject();
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!("", post.content());
         post.approve();
         assert_eq!("I ate a salad for lunch today", post.content());
     }
